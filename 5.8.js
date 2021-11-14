f85bc8f9dc1ccc517564fbf8ca2cdff35c3c8196
yonin7 <yonin789@walla.com>
2021-11-14 09:54:46 +0200

    5.8.js fixed

--- 5.8.js
+++ 5.8.js
@@ -1,7 +1,11 @@
 const shortestWords = (str) => {
   let words = str.split(' ');
   let wordsLength = words.map((ele) => ele.length);
-  return Math.max(...wordsLength);
+  let maxIndex = Math.max(...wordsLength);
+
+  for (let i = 0; i != words.length; i++) {
+    if (wordsLength[i] == maxIndex) return words[i];
+  }
 };
 
 console.log(shortestWords('aa aaaa aaa'));
